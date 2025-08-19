28629b75d3fd0de1f819b524b011454074799c6d
SIDNEY CAMPOS ARAGÃO <25132876@CFP127ALUNO.LOCAL>
2025-08-19 17:00:38 -0300

    Produtos

---
+++ app.js
@@ -0,0 +1,12 @@
+'use script'
+
+import produtos from './produtos.json' with {type:'json'}
+
+
+
+function mostrarProdutos (produto){
+
+}
+produtos.forEach(mostrarProdutos)
+
+console.log(produtos)
